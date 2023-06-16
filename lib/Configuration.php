622d3c915de807f5dca06a6ed3a947578d311010
Pavlo Machekhin <bashmach@gmail.com>
2023-06-16 13:12:20 +0300

    Change default user-agent configuration

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -111,11 +111,11 @@ class Configuration
      */
     protected string $host = 'https://api.pipedrive.com/v1';
     /**
-     * User agent of the HTTP request, set to "OpenAPI-Generator/{version}/PHP" by default
+     * User agent of the HTTP request, set to "Pipedrive-SDK-PHP-5.x" by default
      *
      * @var string
      */
-    protected string $userAgent = 'OpenAPI-Generator/1.0.0/PHP';
+    protected string $userAgent = 'Pipedrive-SDK-PHP-5.x';
 
     /**
      * Debug switch (default set to false)
