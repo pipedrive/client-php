9f7e848c78e79cc0cc1816a5f91bad8880d2d3c0
Volodymyr Khmelyar <vkhmelyar@qualityunit.com>
2023-06-22 11:53:08 +0300

    Use `ReturnTypeWillChange` attribute in `Models\OAuthToken` for PHP8.1 comp

--- src/Models/OAuthToken.php
+++ src/Models/OAuthToken.php
@@ -83,6 +83,7 @@ class OAuthToken implements JsonSerializable
     /**
      * Encode this object to JSON
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         $json = array();
