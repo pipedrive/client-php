65fe2ac6e49c5eafa858b8598e3f0dcef845bea1
Pavlo Machekhin <bashmach@users.noreply.github.com>
2024-05-15 15:12:11 +0300

    Added RawData trait to models to allow access to all response data objects

--- test/Leads/LeadsApiTest.php
+++ test/Leads/LeadsApiTest.php
@@ -46,3 +46,39 @@ it('lists leads', function () {
         ->and($result->getData()[0]->getId())->toEqual(42)
         ->and($result->getData()[0]->getVisibleTo())->toEqual('7');
 });
+
+it('lists leads with raw data', function () {
+    $config = new Configuration();
+
+    $mock = $this->httpMock();
+    $mock->append(
+        new Response(200, [], json_encode([
+            'data' => [
+                [
+                    'id' => 42,
+                    'visible_to' => '7',
+                    '6504f7b19e74a082034fe9b8662383ea6d7cc941' => 'custom'
+                ],
+            ],
+        ])),
+    );
+
+    $handlerStack = HandlerStack::create($mock);
+
+    $apiInstance = new LeadsApi(
+        new Client(['handler' => $handlerStack]),
+        $config,
+    );
+    $result = $apiInstance->getLeads(
+        10,
+        0,
+    );
+
+    $rawData = $result->getRawData();
+
+    expect($rawData)->toBeArray()
+        ->and($rawData[0])->toHaveProperty('id')
+        ->and($rawData[0]->id)->toEqual(42)
+        ->and($rawData[0])->toHaveProperty('6504f7b19e74a082034fe9b8662383ea6d7cc941')
+        ->and($rawData[0]->{'6504f7b19e74a082034fe9b8662383ea6d7cc941'})->toEqual('custom');
+});
