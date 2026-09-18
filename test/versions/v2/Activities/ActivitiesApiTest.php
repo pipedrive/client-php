4e3dac14abe7654ffb6958d163c4c72b411a20f3
Denis <deniss.dmitrijevs@pipedrive.com>
2026-09-18 10:45:28 +0300

    GRAM-4868 fix stale array-param test calls for v2 ids parameter
    getActivities()/getDeals() now type $ids as string[] (array, style
    form, explode false) following the api-docs schema change from
    type: string to type: array. Update the two v2 test calls that were
    still passing a comma-joined string, which throws a TypeError in
    count($ids) with the regenerated SDK.

--- test/versions/v2/Activities/ActivitiesApiTest.php
+++ test/versions/v2/Activities/ActivitiesApiTest.php
@@ -32,7 +32,7 @@ it('lists activities', function () {
         new Client(['handler' => $handlerStack]),
         $config,
     );
-    $result = $apiInstance->getActivities(1, '1,2,3', 1);
+    $result = $apiInstance->getActivities(1, ['1', '2', '3'], 1);
 
     expect($mock->getLastRequest()->getUri()->getQuery())->toEqual('filter_id=1&ids=1%2C2%2C3&owner_id=1&sort_by=id&sort_direction=asc')
         ->and($result->getData())->toHaveLength(1);
